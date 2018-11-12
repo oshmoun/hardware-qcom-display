c3f04d3f31ed3b58b7e4246d67d7370f95e61bc1
oshmoun <oshmoun100@googlemail.com>
2018-11-12 02:59:08 +0100

    Android.mk: Add a build guard to only include for sde devices
    In the current state, the hal depends on an overlying Android.mk to guard it.
    The hal supports SDE but not MSMFB.
    Add a basic build guard to the hal that at least avoids having the hal used for MSMFB devices, which it does not support.

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_USES_SDE),true)
 ifneq ($(TARGET_DISABLE_DISPLAY),true)
 sdm-libs := sdm/libs
 display-hals := include libdebug libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
@@ -17,3 +18,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
 endif
 endif
 endif #TARGET_DISABLE_DISPLAY
+endif
