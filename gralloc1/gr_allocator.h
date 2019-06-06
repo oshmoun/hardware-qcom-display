2ebc5ec4cc5c39cf3e8d530a98cd924c5e436375
oshmoun <oshmoun100@googlemail.com>
2019-06-06 18:43:11 +0200

    gralloc/gralloc1: ion fixes
    Change-Id: I65ceffc34c2c7b39e69a296f47767f8c5265f1d8

--- gralloc1/gr_allocator.h
+++ gralloc1/gr_allocator.h
@@ -52,7 +52,7 @@ class Allocator {
   int MapBuffer(void **base, unsigned int size, unsigned int offset, int fd);
   int ImportBuffer(int fd);
   int FreeBuffer(void *base, unsigned int size, unsigned int offset, int fd, int handle);
-  int CleanBuffer(void *base, unsigned int size, unsigned int offset, int handle, int op);
+  int CleanBuffer(void *base, unsigned int size, unsigned int offset, int handle, int op, int fd);
   int AllocateMem(AllocData *data, gralloc1_producer_usage_t prod_usage,
                   gralloc1_consumer_usage_t cons_usage);
   // @return : index of the descriptor with maximum buffer size req
